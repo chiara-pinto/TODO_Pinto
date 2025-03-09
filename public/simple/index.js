133d812706db4d57f2671fb860fa432449496e1f
Chiara Pinto <pintochiara@itis-molinari.eu>
2025-03-09 18:30:18 +0100

    index.js modificato

--- public/simple/index.js
+++ public/simple/index.js
@@ -1,129 +1,97 @@
 const createMiddleware = () => {
     return {
         send: (todo) => {
-            return new Promise((resolve, reject) => {
-                fetch("/todo/add", {
-                    method: 'POST',
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify(todo)
-                })
-                    .then((response) => response.json())
-                    .then((json) => {
-                        resolve(json); // risposta del server all'aggiunta
-                    })
-            })
+            return fetch("/todo/add", {
+                method: 'POST',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(todo)
+            }).then(response => response.json());
         },
         load: () => {
-            return new Promise((resolve, reject) => {
-                fetch("/todo")
-                    .then((response) => response.json())
-                    .then((json) => {
-                        resolve(json); // risposta del server con la lista         
-                    })
-            })
+            return fetch("/todo").then(response => response.json());
         },
         put: (todo) => {
-            return new Promise((resolve, reject) => {
-                fetch("/todo/complete", {
-                    method: 'PUT',
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify(todo)
-                })
-                    .then((response) => response.json())
-                    .then((json) => {
-                        resolve(json);
-                    })
-            })
+            return fetch("/todo/complete", {
+                method: 'PUT',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(todo)
+            }).then(response => response.json());
         },
         delete: (id) => {
-            return new Promise((resolve, reject) => {
-                fetch("/todo/" + id, {
-                    method: 'DELETE'                
-                })
-                    .then((response) => response.json())
-                    .then((json) => {
-                        resolve(json);
-                    })
-            })
+            return fetch("/todo/" + id, {
+                method: 'DELETE'
+            }).then(response => response.json());
         }
-    }
-}
+    };
+};
 
 const createForm = (add) => {
     const inputInsert = document.querySelector("#inputInsert");
+    const inputDate = document.querySelector("#inputDate");
     const buttonInsert = document.querySelector("#buttonInsert");
     buttonInsert.onclick = () => {
-        add(inputInsert.value);
+        add(inputInsert.value, inputDate.value);
         inputInsert.value = "";
-    }
-}
+        inputDate.value = "";
+    };
+};
 
 const createList = () => {
     const listTable = document.querySelector("#listTable");
     const template = `
-                    <tr>                            
-                        <td class="%COLOR">%TASK</td>
-                        <td><button class="btn btn-success" id="COMPLETE_%ID" type="button">COMPLETA</button></td>                            
-                        <td><button class="btn btn-danger" id="DELETE_%ID" type="button">ELIMINA</button></td>                                                    
-                    </tr>
+        <tr>
+            <td class="%COLOR">%TASK</td>
+            <td>%DATE</td>
+            <td><button class="btn btn-success" id="COMPLETE_%ID">COMPLETA</button></td>
+            <td><button class="btn btn-danger" id="DELETE_%ID">ELIMINA</button></td>
+        </tr>
     `;
     return {
         render: (todos, completeTodo, deleteTodo) => {
             let html = "";
             todos.forEach((todo) => {
-                let row = template.replace("COMPLETE_%ID", "COMPLETE_" + todo.id);
-                row = row.replace("DELETE_%ID", "DELETE_" + todo.id);
-                row = row.replace("%TASK", todo.name);
-                row = row.replace("%COLOR", todo.completed ? "text-success" : "text-primary");
+                let row = template.replace("COMPLETE_%ID", "COMPLETE_" + todo.id)
+                                  .replace("DELETE_%ID", "DELETE_" + todo.id)
+                                  .replace("%TASK", todo.name)
+                                  .replace("%DATE", todo.date)
+                                  .replace("%COLOR", todo.completed ? "text-success" : "text-primary");
                 html += row;
             });
             listTable.innerHTML = html;
             todos.forEach((todo) => {
                 document.querySelector("#COMPLETE_" + todo.id).onclick = () => completeTodo(todo.id);
                 document.querySelector("#DELETE_" + todo.id).onclick = () => deleteTodo(todo.id);
-            })
+            });
         }
-    }
-}
+    };
+};
 
 const createBusinessLogic = (middleware, list) => {
     let todos = [];
     const reload = () => {
-        middleware.load()
-        .then((json) => {
+        middleware.load().then((json) => {
             todos = json.todos;
             list.render(todos, completeTodo, deleteTodo);
-        })
-    }
+        });
+    };
     const completeTodo = (id) => {
-        const todo = todos.filter((todo) => todo.id === id)[0];
-        middleware.put(todo)
-            .then(() => reload());
-    }
-    const deleteTodo =  (id) => {
-        console.log("delete " + id);
-        middleware.delete(id)
-        .then(() => reload());
-    }
+        const todo = todos.find(todo => todo.id === id);
+        middleware.put(todo).then(() => reload());
+    };
+    const deleteTodo = (id) => {
+        middleware.delete(id).then(() => reload());
+    };
     return {
-        add: (task) => {
-            const todo = {
-                name: task,
-                completed: false
-            }
-            middleware.send({ todo: todo })
-                .then(() => reload());
+        add: (task, date) => {
+            const todo = { name: task, completed: false, date: date };
+            middleware.send(todo).then(() => reload());
         },
         reload: reload
-    }
-}
+    };
+};
 
 const middleware = createMiddleware();
 const list = createList();
 const businessLogic = createBusinessLogic(middleware, list);
-const form = createForm(businessLogic.add);
+createForm(businessLogic.add);
 businessLogic.reload();
